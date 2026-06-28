11aec57e645958259958c310e43cda5a455a82d5
David <yerssondavidf@gmail.com>
2026-06-28 17:45:27 -0500

    refactor(assistant): remove test assistant classes and reorganize packages

--- daddyPcBackend/src/main/java/com/example/david/one/daddypcbackend/application/port/out/assistant/IAssistantAITestClient.java
+++
@@ -1,8 +0,0 @@
-package com.example.david.one.daddypcbackend.application.port.out.assistant;
-
-import com.example.david.one.daddypcbackend.application.command.assistant.QuestionToAssistantTestCommand;
-import reactor.core.publisher.Flux;
-
-public interface IAssistantAITestClient {
-    Flux<String> askQuestion(QuestionToAssistantTestCommand command);
-}
