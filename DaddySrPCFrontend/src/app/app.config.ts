ea3d2509efc4ad66bddffe66c52cbd97eab9e355
David <yerssondavidf@gmail.com>
2026-06-28 19:14:55 -0500

    Add ngx-markdown library and GFM configuration

--- DaddySrPCFrontend/src/app/app.config.ts
+++ DaddySrPCFrontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTransloco } from '@jsverse/transloco';
+import { MARKED_OPTIONS, provideMarkdown } from 'ngx-markdown';
 import { TranslocoHttpLoader } from './core/transloco-loader';
 import { routes } from './app.routes';
 
@@ -10,6 +11,16 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
+    provideMarkdown({
+      markedOptions: {
+        provide: MARKED_OPTIONS,
+        useValue: {
+          gfm: true,
+          breaks: false,
+          pedantic: false,
+        },
+      },
+    }),
     provideTransloco({
       config: {
         availableLangs: ['es', 'en', 'pt'],
